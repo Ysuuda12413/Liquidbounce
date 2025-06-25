4d47bbafe6f17ba381cfb4f0085b300807c2ace8
DuyunDz23 <tranvan8911@gmail.com>
2025-06-25 14:06:57 +0700

    remove trash module

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/other/IntaveFlagFly.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/other/IntaveFlagFly.kt
@@ -1,11 +1,11 @@
 /*
  * LiquidBounce Hacked Client
- * IntaveFlyStealth: Fly mode an toàn tối đa cho Intave, ưu tiên không bị flag.
+ * IntaveFlagFly: Fly mode an toàn tối đa cho Intave, ưu tiên không bị flag.
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.flymodes.other
 
 import net.ccbluex.liquidbounce.features.module.modules.movement.flymodes.FlyMode
-import net.ccbluex.liquidbounce.features.module.modules.movement.Fly.maxFlyTicksValue
+import net.ccbluex.liquidbounce.features.module.modules.movement.Fly
 import net.ccbluex.liquidbounce.ui.utils.movement.MovementUtils
 import net.minecraft.network.play.client.C03PacketPlayer
 
@@ -15,11 +15,11 @@ object IntaveFlagFly : FlyMode("IntaveFlagFly") {
     override fun onEnable() {
         tickCount = 0
     }
+
     override fun onUpdate() {
         val player = mc.thePlayer ?: return
         player.motionY = if (tickCount % 12 == 0) 0.022 else -0.012 + ((Math.random() - 0.5) * 0.007)
         player.fallDistance = 0f
-
         if (player.ticksExisted % 3 == 0) {
             player.onGround = true
             mc.netHandler.addToSendQueue(C03PacketPlayer(true))
@@ -28,8 +28,8 @@ object IntaveFlagFly : FlyMode("IntaveFlagFly") {
             MovementUtils.strafe(0.15f + Math.random().toFloat() * 0.02f)
         }
 
-        tickCount++=
-        if (tickCount >= maxFlyTicksValue) {
+        tickCount++
+        if (tickCount >= Fly.maxFlyTicksValue) {
             player.motionY = -0.08
         }
     }
